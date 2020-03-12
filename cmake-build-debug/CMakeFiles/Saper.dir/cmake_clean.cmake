2f92491d427163bd4d320b970a06e82897bb1901
Karol Garbocz <253004@student.pwr.edu.pl>
2020-03-12 16:03:50 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Saper.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Saper.dir/main.cpp.o"
+  "Saper"
+  "Saper.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Saper.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include "cmake-build-debug/class.h"
+
+int main()
+{
+    srand(time(NULL));
+    MinesweeperBoard x(9, 7, DEBUG);
+    x.debug_display();
+    std::cout<<x.getFieldInfo(0,1);
+    return 0;
+}
